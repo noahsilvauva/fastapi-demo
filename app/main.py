c811d6454220490029bdfdc4e7bdf1b197e642d2
Noah Silva <acv7qc@virginia.edu>
2024-03-19 19:48:13 +0000

    API inside of an API

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import json 
+import requests
 # import boto3
 
 app = FastAPI()
@@ -17,6 +19,12 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/github/repos{user}")
+def github_user_repos(user):
+    url = "https://api.github.com/users/" + user + "/repos"
+    response = requests.get(url)
+    body = json.loads(response.text)
+    return {"repos": body}
 
 # Endpoints and Methods
 # /blah - endpoint
